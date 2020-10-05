3d631300f76df98b49d479df546082be313cd5b0
joseph feruglio <josephferuglio@gmail.com>
2020-10-05 11:35:23 -0700

    Added a private variable, constructor, and some methods. Provided Javadoc comments

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -1,4 +1,52 @@
 package cse360assignment02;
 
 public class AddingMachine {
+    private int total; // Declaring private integer
+
+    /**
+     * Constructor method for AddingMachine
+     * Initializes total to 0
+     */
+    public AddingMachine(){
+        total = 0; // not needed - included for clarity
+    }
+
+    /**
+     * Retrieves value of total
+     * @return value of total
+     */
+    public int getTotal(){
+        return 0;
+    }
+
+    /**
+     * Add method to be finished
+     * @param value value to be added
+     */
+    public void add(int value){
+
+    }
+
+    /**
+     * Subtract method to be finished
+     * @param value value to be subtracted
+     */
+    public void subtract (int value){
+
+    }
+
+    /**
+     * toString method to be finished
+     * @return currently returns a blank line
+     */
+    public String toString(){
+        return "";
+    }
+
+    /**
+     * Clear method to be finished
+     */
+    public void clear(){
+
+    }
 }
